b4a0a55e5b00ef22fb24ed45a1e7358db380d029
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-11 11:50:06 +0900

    画像ファイル保存場所の設定

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ] + debug_toolbar_urls()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
