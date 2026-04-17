aa76726616e3698329814f02d40bb14fff52306b
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-17 13:18:28 +0900

    検索機能追加

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views import generic
 
@@ -7,5 +8,15 @@ from .models import Post
 class PostListView(generic.ListView):
     template_name = 'blog/posts/list.html'
     model = Post
-    queryset = Post.objects.select_related('category').order_by('-posted_at')
     paginate_by = 6
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        search = self.request.GET.get('search')
+        if search is not None:
+            queryset = queryset.filter(
+                Q(title__contains=search) | Q(body__contains=search)
+            )
+        
+        return queryset.select_related('category').order_by('-posted_at')
