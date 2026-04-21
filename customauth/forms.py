15823b0bbfcbeb92287719547b708b5bfa70cdc4
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-21 15:32:37 +0900

    一般アカウント登録機能追加

--- customauth/forms.py
+++ customauth/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
 
+from .models import CustomUser
 
 class LoginForm(AuthenticationForm):
     username = forms.EmailField(
@@ -16,3 +18,50 @@ class LoginForm(AuthenticationForm):
             "class": "input w-full"
         })
     )
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('name', 'email')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                "class": "input w-full"
+            }),
+            'email': forms.EmailInput(attrs={
+                "class": "input w-full"
+            })
+        }
+
+    password = forms.CharField(
+        label = 'パスワード',
+        widget=forms.PasswordInput(attrs={
+            "class": "input w-full"
+        })
+    )
+
+    password_confirmation = forms.CharField(
+        label = 'パスワード確認',
+        widget=forms.PasswordInput(attrs={
+            "class": "input w-full"
+        })
+    )
+
+    def clean_password_confirmation(self):
+        # password_confirmation のバリデーション：password と一致しているか
+        password = self.cleaned_data.get('password')
+        password_confirmation = self.cleaned_data.get('password_confirmation')
+        if password and password_confirmation and password != password_confirmation:
+            raise ValidationError("パスワードが一致しません")
+        return password_confirmation
+    
+    def save(self, commit=True):
+        """まず Meta.fields で指定したフィールドで user を作成し、
+        そこに password をセットしてから DB に登録する
+        """
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+    
--- customauth/views.py
+++ customauth/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from .models import CustomUser
 
 class LoginView(BaseLoginView):
     form_class = LoginForm
     template_name = 'customauth/login.html'
+
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = RegisterForm
+    template_name = 'customauth/register.html'
+    success_url = reverse_lazy('customauth:login')
+    
