296b8a8c00c31f9516f6de69810e02ad85f43ed8
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-25 13:16:00 +0900

    AlpineJSをCDNからdjango-alpineに変更

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'theme',
     'django_cotton',
     'blog',
+    'django_alpine',
 ]
 
 if DEBUG:
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
