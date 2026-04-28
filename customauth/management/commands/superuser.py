150d350bbb43e784eae3170ee0caee19634ccdfb
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-28 09:59:54 +0900

    Render デプロイ用設定

---
+++ customauth/management/commands/superuser.py
@@ -0,0 +1,15 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='').exists():
+            User.objects.create_superuser(
+                email=os.environ.get('SUPERUSER_EMAIL'),
+                name=os.environ.get('SUPERUSER_NAME'),
+                password=os.environ.get('SUPERUSER_PASS')
+            )
