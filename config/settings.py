51d1b66030801f5d9a2a235ffa4a0cec4ffb9696
shibamirai <t_shiba@miraino-katachi.co.jp>
2026-04-28 09:44:59 +0900

    Render デプロイ用設定

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&5paq)cz&vm324#or+a7ovz28rbw9-4jvtzsob)h1fm9%xj&n'
+# SECRET_KEY = ''
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = ["127.0.0.1"]
 
---
+++ customauth/management/commands/superuser.py
@@ -0,0 +1,15 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='').exists():
+            User.objects.create_superuser(
+                username=os.environ.get('SUPERUSER_NAME'),
+                email='',
+                password=os.environ.get('SUPERUSER_PASS')
+            )
